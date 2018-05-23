f3cd56cdf2db2144690d9736c150b933aa01614b
Hori <powerkilroy@gmail.com>
2018-05-23 15:53:16 -0400

    comment

--- C#/CaesarCipher/AdvanceCaesarCipher.cs
+++ C#/CaesarCipher/AdvanceCaesarCipher.cs
@@ -1,7 +1,15 @@
 using System;
 
+/*
+
+    Whole point of Advance CaesarCipher is to do everything that CaesarCipher already does, but
+    with more than just letters. This will allow the encoding and decoding of numbers, symbols,
+    letter, punctuation, and even spacing.
+
+ */
+
 class AdvanceCaesarCipher{
     AdvanceCaesarCipher(){
-        
+
     }
 }
