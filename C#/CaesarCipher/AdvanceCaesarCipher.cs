04ce3e788cf27c37855488b1205d5a627308d89e
Hori <powerkilroy@gmail.com>
2018-05-24 11:52:02 -0400

    Added an advance version of CaesarCipher

--- C#/CaesarCipher/AdvanceCaesarCipher.cs
+++ C#/CaesarCipher/AdvanceCaesarCipher.cs
@@ -1,15 +1,53 @@
-using System;
-
-/*
-
-    Whole point of Advance CaesarCipher is to do everything that CaesarCipher already does, but
-    with more than just letters. This will allow the encoding and decoding of numbers, symbols,
-    letter, punctuation, and even spacing.
-
+/*  
+    Created by Timothy Garrett
+    5/24/2018
  */
-
+using System;
+using System.IO;
+/*
+    Takes the input of whatever text document you want and the output document
+    along with a integer and creates a bigger variety of changes in encoding
+    and decoding the documents. All based on ASCII would need to change
+    certain values for it to be useable in Unicode.
+ */
 class AdvanceCaesarCipher{
-    AdvanceCaesarCipher(){
+    public void encode(string input, string output, int numbers){
+        numbers = numbers % 95;
+        StreamReader sr = new StreamReader(input);
+        StreamWriter sw = new StreamWriter(output);
+        while(!sr.EndOfStream){
+            char temp = (char)sr.Read();
+            int mod = temp;
+            if(!Char.IsControl(temp)){
+                if(mod + numbers > 126)
+                    mod = (mod + numbers) - 95;
+                else
+                    mod = mod + numbers;
+                temp = (char) mod;
+            }
+            sw.Write(temp);
+        }
+        sr.Dispose();
+        sw.Dispose();
+    }
 
+    public void decode(string input, string output, int numbers){
+        numbers = numbers % 95;
+        StreamReader sr = new StreamReader(input);
+        StreamWriter sw = new StreamWriter(output);
+        while(!sr.EndOfStream){
+            char temp = (char)sr.Read();
+            int mod = temp;
+            if(!Char.IsControl(temp)){
+                if(mod - numbers < 32)
+                    mod = (mod - numbers) + 95;
+                else
+                    mod = mod - numbers;
+                temp = (char) mod;
+            }
+            sw.Write(temp);
+        }
+        sr.Dispose();
+        sw.Dispose();
     }
 }
--- C#/CaesarCipher/CaesarCipher.cs
+++ C#/CaesarCipher/CaesarCipher.cs
@@ -1,38 +1,42 @@
-﻿using System;
+﻿/*  
+    Created by Timothy Garrett
+    5/23/2018
+ */
+using System;
 using System.IO;
 /*
-
-    ASCII has A..Z as 65->90 and a..z as 97->122.
-
+    Implementation of a simple Caesar Cipher and a more advance Caesar Cipher,
+    if you can call just allowing punctuation and numbers to be in it advance,
+    for encoding text documents and decoding them.All based on ASCII would need 
+    to change certain values for it to be useable in Unicode.
  */
 class CaesarCipher
 {
     static void Main(string[] args)
     {
-        Random rand = new Random();
-        int num = rand.Next() % 26;
-        Console.WriteLine(num);
-        encode(num);
-        decode(num);
+        int rand = new Random().Next();
+        encode(rand);
+        decode(rand);
+        AdvanceCaesarCipher acc = new AdvanceCaesarCipher();
+        acc.encode("input.txt", "output2.txt", rand);
+        acc.decode("output2.txt", "input3.txt", rand);
     }
 
     // Function encode
     static void encode(int num){
+        num = num % 26;
         StreamReader sr = new StreamReader("input.txt");
         StreamWriter sw = new StreamWriter("output.txt");
         while(!sr.EndOfStream){
             char temp = (char)sr.Read();
             int mod = temp;
             if(Char.IsLetter(temp)){
-                if(mod + num > 90 && Char.IsUpper(temp)){
+                if(mod + num > 90 && Char.IsUpper(temp))
                     mod = (mod + num) - 26;
-                }
-                else if(mod + num > 122){
+                else if(mod + num > 122)
                     mod = (mod + num) - 26;
-                }
-                else{
+                else
                     mod = mod + num;
-                }
                 temp = (char) mod;
             }
             sw.Write(temp);
@@ -42,21 +46,19 @@ class CaesarCipher
     }
     // Function decode
     static void decode(int num){
+        num = num % 26;
         StreamReader sr = new StreamReader("output.txt");
         StreamWriter sw = new StreamWriter("input2.txt");
         while(!sr.EndOfStream){
             char temp = (char)sr.Read();
             int mod = temp;
             if(Char.IsLetter(temp)){
-                if(mod - num < 65){
+                if(mod - num < 65)
                     mod = (mod - num) + 26;
-                }
-                else if(mod - num < 97 && Char.IsLower(temp)){
+                else if(mod - num < 97 && Char.IsLower(temp))
                     mod = (mod - num) + 26;
-                }
-                else{
+                else
                     mod = mod - num;
-                }
                 temp = (char) mod;
             }
             sw.Write(temp);
