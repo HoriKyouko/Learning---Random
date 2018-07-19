f870c7f17e574090d412b6c23a4472af5e55f89b
Hori <powerkilroy@gmail.com>
2018-07-19 15:25:13 -0400

    Finished my first Javascript assessment

---
+++ JavaScript/Assessment1/main.js
@@ -0,0 +1,43 @@
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+
+var storyText = "It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+var insertY = ["the soup kitchen", "Disneyland", "the White House"];
+var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+function randomValueFromArray(array){
+    return array[Math.floor(Math.random()*array.length)];
+}
+
+randomize.addEventListener('click', result);
+
+function result() {
+    var newStory = storyText;
+    var xItem = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
+
+    if(customName.value !== '') {
+        var name = customName.value;
+        newStory = newStory.replace("Bob", name);
+    }
+
+    if(document.getElementById("uk").checked) {
+        var weight = Math.round(300 / 14);
+        weight = weight + " stones";
+        var temperature =  Math.round((94 - 32) * (5/9));
+        temperature = temperature + " centigrade";
+        newStory = newStory.replace("94 farenheit", temperature);
+        newStory = newStory.replace("300 pounds", weight);
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+}
