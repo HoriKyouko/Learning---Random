4d82fb05208502395c7d0d99a71ae93de1e61a7c
SurfacePro/Tim <powerkilroy@gmail.com>
2019-06-12 09:00:02 -0400

    Finished QuickSort

--- C/QuickSort.c
+++ C/QuickSort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define length 20
-
+#define cutoff 10
 /**
  * Quick Sort Algorithm as written in Algorithms in a nutshell 2nd Ed.
  * 
@@ -50,20 +50,56 @@ int selectPivot(int *arr, int left, int right){
     return arr[right-1];
 }
 
+void insertionSort(int* arr, int left, int right){
+    int i, j, key;
+    for(i = left+1; i <= right; i++){
+        key = arr[i];
+        for(j = i; j > left && compare(key, arr[j-1]) < 0; j--){
+            arr[j] = arr[j-1];
+        }
+        arr[j] = key;
+    }
+}
+
+void quickSort(int* arr, int left, int right){
+    if(left + cutoff > right){
+        insertionSort(arr, left, right);
+    }
+    else{
+        int pivot = selectPivot(arr, left, right);
+        int i = left, j = right-1;
+        while(1){
+            while(arr[++i] < pivot){}
+            while(pivot < arr[--j]){}
+            if(i < j)
+                swap(arr, i, j);
+            else
+                break;
+        }
+        swap(arr, i, right-1);
+        quickSort(arr, left, i - 1);
+        quickSort(arr, i + 1, right);
+    }
+
+}
+
 int main(){
     // This is completely redundant but I don't want to read from a file in order to get these random
     // numbers. I'm also lazy and don't want to build a unique random number generator.
     int temp[20] = {17, 36, 11, 2, 43, 79, 83, 69, 28, 63, 78, 31, 74, 37, 15, 81, 3, 50, 42, 86};
-    int* quickSort = (int*) malloc(sizeof(int) * length);
+    int* quick = (int*) malloc(sizeof(int) * length);
     int i;
     for(i = 0; i < length; i++)
-        quickSort[i] = temp[i];
+        quick[i] = temp[i];
     
-    
-
+    quickSort(quick, 0, length - 1);
+    /**
+     * Sorted should appear as:
+     * 2 3 11 15 17 28 31 36 37 42 43 50 63 69 74 78 79 81 83 86
+     */
     for(i = 0; i < length; i++)
-        printf("%d ", quickSort[i]);
+        printf("%d ", quick[i]);
 
-    free(quickSort);
+    free(quick);
     return 0;
 }
