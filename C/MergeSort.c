5d0734960086983c1914a08c1a979915f66cc732
Hori <powerkilroy@gmail.com>
2019-07-06 13:18:49 -0400

    Fixed duplicates problem with MergeSort

--- C/MergeSort.c
+++ C/MergeSort.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define length 7
+#define length 20
 
 /**
  * Merge Sort Algorithm as written in Algorithms in a nutshell 2nd Ed.
  * 
  * EDIT: Found out later that this doesn't accept duplicates in the
  * array. Will need to find a work around for it...
+ * 
+ * EDIT2: Found a solution for allowing duplicates, but had to change up
+ * my Omega(N) space complexity, but also allowed for a WAY simpler
+ * solution that is easy to follow and understand.
  * 
  * Runtime:
  * 
@@ -31,53 +35,65 @@ int compare(int a, int b){
         return 1;
 }
 
-void mergeSort(int* copy, int* array, int start, int end){
-    if(end - start < 2)
-        return;
+void merge(int *array, int start, int mid, int end){
+    int i, j, index = start;
+    int len1 = mid - start + 1;
+    int len2 = end - mid;
+    int *L = (int*)calloc(len1, sizeof(int));
+    int *R = (int*)calloc(len2, sizeof(int));
 
-    if(end-start == 2){
-        if(array[start] > array[start+1]){
-            int temp = array[start];
-            array[start] = array[start+1];
-            array[start+1] = temp;
-        }
-        return;
+    for(i = 0; i < len1; i++){
+        L[i] = array[start+i];
     }
+    for(j = 0; j < len2; j++){
+        R[j] = array[mid+j+1];
+    }
+    i = j = 0;
 
-    int mid = (end+start)/2;
-    mergeSort(copy, array, start, mid);
-    mergeSort(copy, array, mid+1, end);
-
-    int i = start, j = mid, index = start;
-    while(index < end){
-        if(j >= end || (i < mid && copy[i] < copy[j])){
-            array[index] = copy[i];
+    while(i < len1 && j < len2){
+        if(L[i] <= R[j]){
+            array[index] = L[i];
             i++;
         }
         else{
-            array[index] = copy[j];
+            array[index] = R[j];
             j++;
         }
         index++;
     }
+
+    while(i < len1){
+        array[index] = L[i];
+        i++;
+        index++;
+    }
+    while(j < len2){
+        array[index] = R[j];
+        j++;
+        index++;
+    }
 }
 
-void sort(int* array){
-    int* copy = array;
-    mergeSort(copy, array, 0, length);
+void mergeSort(int* array, int start, int end){
+    if(start < end){
+        int mid = (end+start)/2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid+1, end);
+        merge(array, start, mid, end);
+    }
 }
 
 int main(){
     // This is completely redundant but I don't want to read from a file in order to get these random
     // numbers. I'm also lazy and don't want to build a unique random number generator.
-    //int temp[20] = {17, 36, 11, 2, 43, 79, 83, 69, 28, 63, 78, 31, 74, 37, 15, 81, 3, 50, 42, 86};
-    int temp[7] = {2,2,5,1,4,3,7};
+    int temp[] = {17, 36, 11, 2, 43, 79, 83, 69, 28, 63, 78, 31, 74, 37, 15, 81, 3, 50, 42, 86};
+    //int temp[] = {2,2,5,1,4,3,7};
     int* array = (int*) malloc(sizeof(int) * length);
     int i;
     for(i = 0; i < length; i++)
         array[i] = temp[i];
     
-    sort(array);
+    mergeSort(array, 0, length-1);
 
     for(i = 0; i < length; i++)
         printf("%d ", array[i]);
