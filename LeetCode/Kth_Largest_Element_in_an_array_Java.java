bf70428154f5bd1b579c8854c1a0c0f5dfe1087c
Hori <powerkilroy@gmail.com>
2020-02-26 19:15:34 -0500

    Completed Kth largest element in an array in Java

---
+++ LeetCode/Kth_Largest_Element_in_an_array_Java.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums.length == 0)
+            return -1;
+        Arrays.sort(nums);
+        return nums[nums.length -k];
+    }
+}
