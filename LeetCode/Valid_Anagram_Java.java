6bf6e9342fe1436d11f9499cbc192d3b7e546a01
Hori <powerkilroy@gmail.com>
2020-04-10 21:47:04 -0400

    Completed Valid Anagram in Java

---
+++ LeetCode/Valid_Anagram_Java.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int [] count = new int[26];
+        for(char c: s.toCharArray()) count[c-'a']++;
+        
+        for(char c: t.toCharArray()) count[c-'a']--;
+        
+        for(int i: count) if(i != 0) return false;
+        
+        return true;
+    }
+}
