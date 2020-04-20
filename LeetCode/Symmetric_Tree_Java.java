ade3d6c0b1593c2d749d5ad8507edc5dfa8d3f97
Hori <powerkilroy@gmail.com>
2020-04-20 15:09:24 -0400

    Completed Symmetric Tree in Java

---
+++ LeetCode/Symmetric_Tree_Java.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root, root);
+    }
+    
+    private boolean helper(TreeNode r1, TreeNode r2){
+        if(r1 == null && r2 == null) return true;
+        else if(r1 == null || r2 == null) return false;
+        
+        return (r1.val == r2.val) && helper(r1.left, r2.right) && helper(r1.right, r2.left);
+    }
+}
