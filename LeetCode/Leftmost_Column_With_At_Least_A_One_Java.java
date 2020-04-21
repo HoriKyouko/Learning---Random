d6d914763aeba4d1fb9ada45b8548a5c98818061
Hori <powerkilroy@gmail.com>
2020-04-21 10:02:06 -0400

    Completed Leftmost column with at least a one in java

---
+++ LeetCode/Leftmost_Column_With_At_Least_A_One_Java.java
@@ -0,0 +1,32 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int x, int y) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
+
+class Solution {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> size = binaryMatrix.dimensions();
+        int[][] memo = new int[size.get(0)][size.get(1)];
+        int x = 0;
+        int y = size.get(1)-1;
+        
+        int output = -1;
+        
+        while(x < size.get(0) && y >= 0){
+            int val = binaryMatrix.get(x,y);
+            if(val == 1){
+                output = y;
+                y--;
+            }
+            else{
+                x++;
+            }
+        }
+        
+        return output;
+    }
+}
