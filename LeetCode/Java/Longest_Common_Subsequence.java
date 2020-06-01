012b0309726282a3e8b3d6217126456e2df8cfd2
Hori <powerkilroy@gmail.com>
2020-05-31 21:32:13 -0400

    Completed Longest Common Subsequence in Java

---
+++ LeetCode/Java/Longest_Common_Subsequence.java
@@ -0,0 +1,18 @@
+public class Longest_Common_Subsequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int [][] dp = new int[len1+1][len2+1];
+        for(int i = 0; i < len1; i++){
+            for(int j = 0; j < len2; j++){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                }
+                else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
