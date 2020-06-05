54e2c45e4de5fd6d191bc92f5a287b68058d6a1e
Hori <powerkilroy@gmail.com>
2020-06-04 21:46:05 -0400

    Completed Check If N and it's Double Exist.

---
+++ LeetCode/Java/misc/Check_If_N_and_its_Double_Exist.java
@@ -0,0 +1,13 @@
+public class Check_If_N_and_its_Double_Exist {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num: arr){
+            if(set.contains(num * 2) || num % 2 == 0 && set.contains(num / 2))
+                return true;
+            set.add(num);
+        }
+            
+        return false;
+    }
+}
