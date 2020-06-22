ed52ce5fcbaaba5d5421971687a65def4824c77e
Hori <powerkilroy@gmail.com>
2020-06-22 08:45:50 -0400

    Completed Single Number 2 in Java

---
+++ LeetCode/Java/June2020/week4/Single_Number2.java
@@ -0,0 +1,11 @@
+public class Single_Number2 {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: nums)
+            map.put(i, map.getOrDefault(i, 0)+1);
+        for(int i: nums)
+            if(map.get(i)== 1)
+                return i;
+        return 0;
+    }
+}
