20767418b34f1997885b6b1dd487f29af29d46d8
Hori <powerkilroy@gmail.com>
2020-07-05 20:34:04 -0400

    Completed Hamming Distance in Java

---
+++ LeetCode/Java/July2020/week1/Hamming_Distance.java
@@ -0,0 +1,11 @@
+public class Hamming_Distance {
+    public int hammingDistance(int x, int y) {
+        int dist = 0;
+        int val = x ^ y;
+        while(val != 0){
+            dist++;
+            val &= val - 1;
+        }
+        return dist;
+    }
+}
