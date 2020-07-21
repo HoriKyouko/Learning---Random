7a6d5b86c440a3f25fa0c19e2a3427c9a1ec1f69
Hori <powerkilroy@gmail.com>
2020-07-21 11:55:48 -0400

    Updated word search to be more optimized.

--- LeetCode/Java/July2020/week3/Word_Search.java
+++ LeetCode/Java/July2020/week3/Word_Search.java
@@ -1,14 +1,12 @@
 public class Word_Search {
     boolean[][] visited;
     public boolean exist(char[][] board, String word) {
+        // dfs from start and see if word exists. if so return true;
         visited = new boolean[board.length][board[0].length];
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                // dfs from start and see if word exists.
-                // if so return true;
-                if(board[i][j] == word.charAt(0))
-                    if(dfs(board, i, j, word, 0))
-                        return true;
+            for(int j = 0; j < board[1].length; j++){
+                if(board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0))
+                    return true;
             }
         }
         return false;
@@ -17,17 +15,11 @@ public class Word_Search {
     private boolean dfs(char[][] board, int i, int j, String word, int pos){
         if(pos == word.length())
             return true;
-        
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(pos) || visited[i][j]){
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(pos) || visited[i][j])
             return false;
-        }
+            
         visited[i][j] = true;
-        boolean top = dfs(board, i+1, j, word, pos+1);
-        boolean bottom = dfs(board, i-1, j, word, pos+1);
-        boolean left = dfs(board, i, j-1, word, pos+1);
-        boolean right = dfs(board, i, j+1, word, pos+1);
-        
-        if(top || bottom || left || right)
+        if(dfs(board, i+1, j, word, pos+1) || dfs(board, i-1, j, word, pos+1) || dfs(board, i, j-1, word, pos+1) || dfs(board, i, j+1, word, pos+1))
             return true;
         
         visited[i][j] = false;
