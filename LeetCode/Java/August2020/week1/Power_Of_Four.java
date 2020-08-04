e10f39eb5c867ba7fbba713a052d8f01e9b747b9
Hori <powerkilroy@gmail.com>
2020-08-04 10:53:15 -0400

    Completed Power Of Four in Java

---
+++ LeetCode/Java/August2020/week1/Power_Of_Four.java
@@ -0,0 +1,15 @@
+public class Power_Of_Four {
+    public boolean isPowerOfFour(int num) {
+        int biggestPowerOfFour = 1073741824;
+        if(num > biggestPowerOfFour)
+            return false;
+        int sum = 1;
+        int count = 1;
+        while (sum <= num){
+            if(sum == num)
+                return true;
+            sum <<= 2;
+        }
+        return false;
+    }
+}
