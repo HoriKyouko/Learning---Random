57a2c14faddb887ee25887269f3e865fbf945b55
Hori <powerkilroy@gmail.com>
2020-08-26 10:45:55 -0400

    Completed Fizz Buzz in Java

--- LeetCode/Java/August2020/week4/Fizz_Buzz.java
+++ LeetCode/Java/August2020/week4/Fizz_Buzz.java
@@ -2,18 +2,14 @@ public class Fizz_Buzz {
     public List<String> fizzBuzz(int n) {
         List<String> output = new ArrayList<>();
         for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 && i % 5 == 0){
+            if(i % 3 == 0 && i % 5 == 0)
                 output.add("FizzBuzz");
-            }
-            else if(i % 5 == 0){
+            else if(i % 5 == 0)
                 output.add("Buzz");
-            }
-            else if(i % 3 == 0){
+            else if(i % 3 == 0)
                 output.add("Fizz");
-            }
-            else{
+            else
                 output.add(Integer.toString(i));
-            }
         }
         return output;
     }
